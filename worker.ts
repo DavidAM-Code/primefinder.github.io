0efc37915ce75a21316f5049f8f3dff235b8aee6
David Murach <76108449+IMACYD@users.noreply.github.com>
2023-03-22 13:26:20 +0100

    Add files via upload

---
+++ worker.ts
@@ -0,0 +1,11 @@
+onmessage = (e) => {
+    console.log("Message received from main script");
+    const workerResult = `Result: ${e.data[0] * e.data[1]}`;
+    let o = 0
+    for (let i = 0; i < 10000*10000; i++){
+        o += 1
+    }
+    setTimeout(() => {  console.log(o); }, 5000);
+    console.log("Posting message back to main script");
+    postMessage(workerResult);
+};
